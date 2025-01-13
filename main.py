f4f1fbe07419a4a834bd6ffe93625bb384ff84d7
MChenFK <michaelchen1923@gmail.com>
2025-01-13 18:32:41 -0500

    created circleshape and player class and subclass

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pygame
 from constants import *
+from player import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init
+    clock = pygame.time.Clock()
+    dt = 0
+    framerate = 60
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,7 +23,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0), rect=None, special_flags=0)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(framerate) / 1000
 
 if __name__ == "__main__":
     main()
